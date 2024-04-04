ab3fdd60b6fd47e1aebbd9c18ef90ca7cbb1c9bb
Dean Mikalajczyk <131013445+Dmikalaj@users.noreply.github.com>
2024-04-04 09:55:00 -0400

    AzureGetSecurityGroupEmails.ps1

--- AzureGetSecurityGroupEmails.ps1
+++ AzureGetSecurityGroupEmails.ps1
@@ -1,4 +1,4 @@
-#Script to get all the email addresses of the members of a security group in azure
+#Script to get all the members/email addresses of a security group in azure
 Connect-AzureAD
 ###
-Get-AzureADGroupMember -ObjectId "" | select mail | Export-Csv -path 'c:\temp\emails.csv'
+Get-AzureADGroup -ObjectId 'OBJECTID' | Get-AzureADGroupMember -All $True  | Export-Csv -path 'c:\temp\emails.csv'
