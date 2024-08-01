1c21ef5963fb73cf016efe721ed5fa24c2c18efc
Dean Mikalajczyk <131013445+Dmikalaj@users.noreply.github.com>
2024-08-01 16:38:24 -0400

    Create RecurseAndDeleteFiles.ps1

---
+++ RecurseAndDeleteFiles.ps1
@@ -0,0 +1,23 @@
+# Specify the root directory from which to start recursing
+$rootDirectory = "C:\Windows\ccmcache"
+
+# Change to the files to be deleted
+$fileNamesToDelete = @("connections.json", "install.ps1")
+
+# Recursively get files matching the specified names and delete them
+foreach ($fileName in $fileNamesToDelete) {
+    $filesToDelete = Get-ChildItem -Path $rootDirectory -Filter $fileName -File -Recurse
+    if ($filesToDelete) {
+        foreach ($file in $filesToDelete) {
+            try {
+                Remove-Item -Path $file.FullName -Force -ErrorAction Stop
+                Write-Host "File removed successfully: $($file.FullName)"
+            } catch {
+                Write-Host "Failed to remove file: $($file.FullName)"
+                Write-Host $_.Exception.Message
+            }
+        }
+    } else {
+        Write-Host "No files found matching pattern: $fileName in directory: $rootDirectory"
+    }
+}
